0dc684e878929c9770467ebffee3a67c28f623f6
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2021-11-30 00:03:16 -0500

    adding new mix project

---
+++ lib/advent_of_code.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode do
+  @moduledoc """
+  Documentation for `AdventOfCode`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode.hello()
+      :world
+
+  """
+  def main(_args) do
+    IO.puts("hello world")
+  end
+end
